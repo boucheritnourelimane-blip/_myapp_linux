7f5de5871da584190de4690f93fb75eca597e687
nour <boucheritnourelimane@gmail.com>
2026-05-02 03:56:59 +0100

    fix app.py and tests passing

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return "Hello from Linux Server 🚀"
 
+@app.route('/status')
+def status():
+    return {"status": "running"}
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
